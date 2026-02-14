5cb3ec9834d4e9c93a4c4f190a71960271a8b0e9
Graves067 <c.a.gillespie9@gmail.com>
2026-02-13 19:50:08 -0800

    Update Roboflow_build.py

--- src/Roboflow_build.py
+++ src/Roboflow_build.py
@@ -1,6 +1,7 @@
 from roboflow import Roboflow
 
 
+rf = Roboflow(api_key="8fEH8vuftp3sDqrsyOSD")
 print("Connecting to workspace...")
 project = rf.workspace("object-detection-using-roboflow-and-active-learning").project("dice-03h7s")
 print("Project loaded, downloading dataset...")
