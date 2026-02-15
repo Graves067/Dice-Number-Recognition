3a459a745a974f4fcc3de2b7a25a11928513c473
Graves067 <c.a.gillespie9@gmail.com>
2026-02-14 23:53:43 -0800

    Update config.py

--- src/config.py
+++ src/config.py
@@ -8,6 +8,4 @@ PADDING        = 20     #  padding for around bounding box
 
 # OCR settings
 OCR_CONFIDENCE = 0.5    # minimum OCR confidence 
-OCR_GPU        = False  # GPU accelerated OCR
-
-#test for git commit
+OCR_GPU        = True  # GPU accelerated OCR
